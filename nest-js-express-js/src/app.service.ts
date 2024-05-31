32a2a01f56253a34747b715e2af5066602f7d1ad
isabla <blais.isael.entr@gmail.com>
2024-05-30 22:13:06 -0400

    Call random-user api using axios service

--- nest-js-express-js/src/app.service.ts
+++ nest-js-express-js/src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
+import {HttpService} from "@nestjs/axios";
+import {catchError, firstValueFrom} from "rxjs";
+import {AxiosError} from "axios";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(AppService.name);
+  constructor(private readonly httpService: HttpService) {}
+
+  async findRandomUser(): Promise<any> {
+    const { data } = await firstValueFrom(
+        this.httpService.get<any>('https://randomuser.me/api/').pipe(
+            catchError((error: AxiosError) => {
+              this.logger.error(error?.response?.data);
+              throw 'An error happened!';
+            }),
+        ),
+    );
+    return data;
   }
 }
